bdd3a28d25ac8b4528dce66de3edf8695eaa604a
Mauro Leonardi <mauro.leonardi@hotmail.com>
2023-12-10 16:19:39 +1100

    nextJS_dashboardComplete

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { monserrat } from './ui/fonts';
+import './ui/global.css'
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Mauro`s Dashboard',
+    default: 'Mauro`s Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${monserrat.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
